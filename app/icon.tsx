515a8198aa29eecb99f8d990ce696971e2404bfc
notfakedan-web <notfakedan@gmail.com>
2026-02-03 17:39:46 -0500

    Create icon.tsx

---
+++ app/icon.tsx
@@ -0,0 +1,41 @@
+import { ImageResponse } from 'next/og';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: '#06b6d4', // Your Cyan-500
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'black', // Text color
+          borderRadius: '50%', // Circle shape
+          fontWeight: 900,
+          fontStyle: 'italic',
+        }}
+      >
+        V
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  );
+}
